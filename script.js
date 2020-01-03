f3acdba9cfe44f11a90040e3d73c1f861c9ca651
Joel D'Autilio <joeldautilio@gmail.com>
2020-01-03 18:54:08 -0300

    UI Rock Paper Scissors final state

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let computerPoints = 0;
+let playerPoints = 0;
+const buttons = document.querySelectorAll('button');
+let round = 1;
+
+// Makes a random choice for computer's turn
 function computerPlay() {
     let rand = Math.floor(Math.random()*9)+1;
     return (rand <= 3) ? 'Rock' :
@@ -5,38 +11,60 @@ function computerPlay() {
            'Scissors';
 }
 
-function playRound(playerSelection, computerSelection) {
-    player = playerSelection.toLowerCase();
-    computer = computerSelection.toLowerCase();
+// Alters the HTML to show the state of the game
+function displayRound(playerPlay, computerPlay, result, round) {
+    const roundDisp = document.querySelector('.round');
+    const game = document.querySelector('.game');
+    const points = document.querySelector('.points');
 
-    if (player == 'rock') {
-        if (computer == 'rock') return 'tie';
-        else if (computer =='paper') return 'computer';
-        else return 'player';
-    }
-    else if (player == 'paper') {
-        if (computer == 'rock') return 'player';
-        else if (computer == 'paper') return 'tie';
-        else return 'computer';
+    roundDisp.textContent = `Round ${round}`;
+    game.innerHTML = `Player plays <b>${playerPlay}</b>` + '<br>' +
+            `Computer plays <b>${computerPlay}</b>` + '<br>' +
+            result.charAt(0).toUpperCase() + result.slice(1);
+    if (playerPoints < 5 && computerPoints < 5) {
+        points.textContent = `Player ${playerPoints} - ${computerPoints} Computer`;
     }
     else {
-        if(computer == 'rock') return 'computer';
-        else if (computer == 'paper') return 'player';
-        else return 'tie';
+        points.textContent = 'Winner: ' + result.toUpperCase(); + '!!';
     }
 }
 
-function game() {
-    playerPoints = computerPoints = 0;
-    for (let i = 0; i < 5; i++) {
-        playerSelection = prompt('Rock, Paper or Scissors?');
-        computerSelection = computerPlay();
+// Takes both parts' choices and informs the winner
+function playRound(playerSelection, computerSelection) {
+    let player = playerSelection.toLowerCase();
+    let computer = computerSelection.toLowerCase();
+    let result;
 
-        roundWinner = playRound(playerSelection, computerSelection);
-        if (roundWinner == 'player') playerPoints++;
-        else if(roundWinner == 'computer') computerPoints++;
+    if (player == 'rock') {
+        if (computer == 'rock') result = 'tie';
+        else if (computer =='paper') result = 'computer';
+        else result = 'player';
     }
-    return (playerPoints > computerPoints) ? 'Player wins!' :
-           (playerPoints < computerPoints) ? 'Computer wins!' :
-           'It\'s a tie!'
+    else if (player == 'paper') {
+        if (computer == 'rock') result = 'player';
+        else if (computer == 'paper') result = 'tie';
+        else result = 'computer';
+    }
+    else {
+        if(computer == 'rock') result = 'computer';
+        else if (computer == 'paper') result = 'player';
+        else result = 'tie';
+    }
+    
+    // If a player reached 5 points, restarts the game
+    if (playerPoints < 5 && computerPoints < 5) {
+        round++;
+        if (result == 'computer') computerPoints++;
+        else if (result == 'player') playerPoints++;
+    }
+    else {
+        round = 1;
+        computerPoints = playerPoints = 0;
+    }
+    displayRound(player, computer, result, round);
+    return result;
 }
+
+buttons.forEach(button => button.addEventListener('click', () => {
+    playRound(`${button.id}`, computerPlay())
+}));
